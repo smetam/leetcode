0cb54d9a4125b6e87b991f645ae9f18179c36eb0
Smetanin Aleksandr <smetamx@gmail.com>
2022-08-22 22:01:02 +0200

    update 0006 solution

--- problems/0006_zigzag_conversion/zigzag_conversion.py
+++ problems/0006_zigzag_conversion/zigzag_conversion.py
@@ -1,6 +1,7 @@
 class Solution:
-
-    def get_next_row_and_delta(self, current_row: int, num_rows: int, delta: int) -> int:
+    
+    @staticmethod
+    def get_next_row_and_delta(current_row: int, num_rows: int, delta: int) -> int:
         if (num_rows == 1) or (delta == 0):
             return 0, 0
         if current_row == 0:
@@ -10,27 +11,11 @@ class Solution:
         return current_row + delta, delta
 
     def convert(self, s: str, numRows: int) -> str:
-        rows = {i: '' for i in range(numRows)}
+        rows = ['' for i in range(numRows)]
         current_row = 0
         delta = 1
-        for i, symbol in enumerate(s):
+        for symbol in s:
             rows[current_row] += symbol
-            delta
             current_row, delta = self.get_next_row_and_delta(current_row, numRows, delta)
-        result = ''
-        for i in range(numRows):
-            result += rows[i]
+        result = ''.join(rows)
         return result
-
-
-
-class Solution2:
-
-    def convert(self, s: str, numRows: int) -> str:
-        result = s[0]
-        current_row = 0
-        current_pos = 0
-        for i in range(1, len(s)):
-            next_pos = 
-
-
