a29fd7a08aaaa53e21c29ba20b600295fb7c53fc
Smetanin Aleksandr <smetamx@gmail.com>
2023-01-08 09:07:37 +0100

    add 0001 tests

---
+++ problems/0001_two_sum/test_two_sum.py
@@ -0,0 +1,10 @@
+import pytest
+from two_sum import Solution
+
+@pytest.mark.parametrize('nums, target, expected', [
+    ([2,7,11,15], 9, (0,1)),
+    ([3,2,4], 6, (1,2)),
+    ([3,3], 6, (0,1)),
+])
+def test_solution(nums, target, expected):
+    assert Solution().twoSum(nums, target) == expected
