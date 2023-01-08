ebaa06c0e5047284b0a1c20d2e957ef41e338c61
Smetanin Aleksandr <smetamx@gmail.com>
2023-01-08 09:45:59 +0100

    add 0088

---
+++ problems/0088_merge_sorted_array/merge_sorted_array.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        idx1, idx2 = m - 1, n - 1
+        idx = m + n - 1
+        while idx2 >= 0:
+            if idx1 < 0:
+                for i in range(idx2+1):
+                    nums1[i] = nums2[i]
+                idx2 = -1
+                continue
+        
+            n1 = nums1[idx1]
+            n2 = nums2[idx2]
+            if n1 <= n2:
+                nums1[idx] = n2
+                idx2 -= 1
+            else:
+                nums1[idx] = n1
+                idx1 -= 1
+            idx -= 1
+
+        # Uncomment this to run tests
+        # return nums1
---
+++ problems/0088_merge_sorted_array/test_merge_sorted_array.py
@@ -0,0 +1,11 @@
+import pytest
+from merge_sorted_array import Solution
+
+@pytest.mark.parametrize('nums1, m, nums2, n, expected', [
+    ([1,2,3,0,0,0], 3, [2,5,6], 3, [1,2,2,3,5,6]),
+    ([1], 1, [], 0, [1]),
+    ([0], 0, [1], 1, [1]),
+    ([2,0], 1, [1], 1, [1, 2]),
+])
+def test_solution(nums1, m, nums2, n, expected):
+    assert Solution().merge(nums1, m, nums2, n) == expected
