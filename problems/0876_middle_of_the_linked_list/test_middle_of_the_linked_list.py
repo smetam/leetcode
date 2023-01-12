1c9f410c95db8d11ded2ef60c1e23d8ed28e4722
Smetanin Aleksandr <smetamx@gmail.com>
2023-01-12 19:48:39 +0100

    update 0876

--- problems/0876_middle_of_the_linked_list/test_middle_of_the_linked_list.py
+++ problems/0876_middle_of_the_linked_list/test_middle_of_the_linked_list.py
@@ -3,7 +3,15 @@ from middle_of_the_linked_list import Solution, ListNode
 
 
 def array_to_linked_list(arr: list) -> ListNode:
-    ...
+    if len(arr) == 0:
+        return None
+    head = ListNode(val=arr[0])
+    prev = head
+    for val in arr[1:]:
+        node = ListNode(val=val)
+        prev.next = node
+        prev = node
+    return head
 
 
 @pytest.mark.parametrize('arr, expected', [
@@ -13,4 +21,7 @@ def array_to_linked_list(arr: list) -> ListNode:
 def test_solution(arr, expected):
     node = array_to_linked_list(arr)
     expected = array_to_linked_list(expected)
+    print(node)
+    print(expected)
+    print(Solution().middleNode(node))
     assert Solution().middleNode(node) == expected
