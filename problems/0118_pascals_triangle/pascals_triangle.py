59f593a8aea40f365ded7d24b70cc12626113c58
Smetanin Aleksandr <smetamx@gmail.com>
2023-01-22 20:58:08 +0100

    update 0118

--- problems/0118_pascals_triangle/pascals_triangle.py
+++ problems/0118_pascals_triangle/pascals_triangle.py
@@ -6,7 +6,6 @@ class Solution:
         pascals_triangle = [[1]]
         for _ in range(1, numRows):
             previous_row = pascals_triangle[-1]
-            print(previous_row)
             current_row = [sum(pair) for pair in zip(previous_row[1:], previous_row[:-1])]
             current_row = [1] + current_row + [1]
             pascals_triangle.append(current_row)
