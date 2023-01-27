d727920930a65357cbd0b7d31e02c54db356115e
Smetanin Aleksandr <smetamx@gmail.com>
2023-01-27 11:57:37 +0100

    update 0322

--- problems/0322_coin_change/coin_change.py
+++ problems/0322_coin_change/coin_change.py
@@ -3,7 +3,6 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        coins.sort(reverse=True)
         dp = [0] + [-1 for _ in range(amount)]
         for am in range(1, amount + 1):
             coins_counts = [dp[am - coin] for coin in coins if am - coin >= 0]
