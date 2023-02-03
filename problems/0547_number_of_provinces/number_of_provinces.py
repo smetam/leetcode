1f59685e2c20aeb4fb9fd3b033079005e2b798bc
Smetanin Aleksandr <smetamx@gmail.com>
2023-02-03 12:44:54 +0100

    add new solution for 0547

--- problems/0547_number_of_provinces/number_of_provinces.py
+++ problems/0547_number_of_provinces/number_of_provinces.py
@@ -4,6 +4,33 @@ from typing import List
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n_cities = len(isConnected)
+        visited = set()
+        def dfs(start_city: int):
+            q = [start_city]
+            visited.add(start_city)
+            while len(q) > 0:
+                city = q.pop()
+                for neighbour, is_connected in enumerate(isConnected[city]):
+                    if neighbour in visited:
+                        continue
+
+                    if is_connected:
+                        visited.add(neighbour)
+                        q.append(neighbour)
+
+        n_provinces = 0
+        for city in range(n_cities):
+            if city in visited:
+                continue
+
+            dfs(city)
+            n_provinces += 1
+        return n_provinces
+
+
+class Solution1:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n_cities = len(isConnected)
         city2province = {x: x for x in range(n_cities)}
         for row in range(0, n_cities - 1):
             for col in range(row + 1, n_cities):
